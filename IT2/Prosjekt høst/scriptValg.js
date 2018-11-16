244395a4fc9627a256871af2ff9064f001d2a350
majaaustin <fama1312@akershus-fk.no>
2018-11-16 12:09:31 +0100

    jk

--- IT2/Prosjekt høst/scriptValg.js
+++ IT2/Prosjekt høst/scriptValg.js
@@ -1,15 +1,24 @@
 var pTekst = document.getElementById("pTekst")
 var btnSvar2 = document.getElementById("btnSvar2");
-var tekstliste = ["Hi!", "My name is Peter", "You must be new around here?", "What is your name?"];
+var inpTekst = document.getElementById("inpTekst");
+var btnTilInp = document.getElementById("btnTilInp");
+var tekstliste = ["Hi!", "My name is Peter", "You must be new around here?", "What is your name?", "Nice to meet you"];
 let n = 0;
+let m = 0;
 btnSvar2.onclick = function () {
+
     n++;
     if (n>3){
-        n = 0;
+        n=0;
+
+        pTekst.innerHTML = tekstliste[n];
     }
-    pTekst.innerHTML = tekstliste[n];
+    if (n=3){
+        inpTekst.type = "text";
+        btnTilInp.style.visibility = "visible";
+    }
 
-
+    console.log(n);
 }
 
 
