68d76ace724a8c458619cc2fec15720a02f73c09
majaaustin <fama1312@akershus-fk.no>
2018-11-16 13:10:00 +0100

    laget input felt som skal passe til tekstliste fikset css

--- IT2/Prosjekt høst/scriptValg.js
+++ IT2/Prosjekt høst/scriptValg.js
@@ -1,24 +1,28 @@
-var pTekst = document.getElementById("pTekst")
+var pTekst = document.getElementById("pTekst");
 var btnSvar2 = document.getElementById("btnSvar2");
 var inpTekst = document.getElementById("inpTekst");
 var btnTilInp = document.getElementById("btnTilInp");
-var tekstliste = ["Hi!", "My name is Peter", "You must be new around here?", "What is your name?", "Nice to meet you"];
+var tekstliste = ["Hi!", "My name is Peter", "You must be new around here?", "What is your name?", "Nice to meet you" ];
 let n = 0;
 let m = 0;
 btnSvar2.onclick = function () {
-
+    pTekst.innerHTML = tekstliste[n];
     n++;
-    if (n>3){
+    if (n>5){
         n=0;
 
         pTekst.innerHTML = tekstliste[n];
     }
-    if (n=3){
+    if (n===4){
         inpTekst.type = "text";
         btnTilInp.style.visibility = "visible";
     }
 
+
     console.log(n);
+}
+//btnTilInp.onclick = function () {
+    //inpTekst.value = ;
 }
 
 
